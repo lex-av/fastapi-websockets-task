324699e17bc55c9e9592755df07c00ee5ba2a868
alex <allexander0509@gmail.com>
2022-07-20 13:56:34 +0300

    Added static files support for css. Added style for list to make it unmarked

--- src/server_utility/app.py
+++ src/server_utility/app.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from starlette.responses import FileResponse
+from pathlib import Path
+
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 main_app = FastAPI(
     title="messages",
@@ -7,10 +10,19 @@ main_app = FastAPI(
     version="1.0.0",
 )
 
+main_app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
+    name="static",
+)
+
+templates = Jinja2Templates(directory="client_utility")
+
 
 @main_app.get("/")
-async def root():
-    return FileResponse("client_utility/main_page_ws.html")
+async def root(request: Request):
+    # return FileResponse("client_utility/main_page_ws.html")
+    return templates.TemplateResponse("main_page_ws.html", {"request": request})
 
 
 @main_app.websocket("/ws")
